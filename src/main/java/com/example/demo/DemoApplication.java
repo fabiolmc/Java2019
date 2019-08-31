f7801fc9b9406b5d40e93aa8494cefde2f312fe7
patote85 <R.patote@gmail.com>
2019-08-31 09:18:09 -0300

    atualizacao do projeto

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-// area de importação de bibliotecas
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
